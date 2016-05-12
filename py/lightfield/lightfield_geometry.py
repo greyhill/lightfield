69ad15ce0936b838a597f47081f7dffa7d412fcb
Madison McGaffin <greyhill@gmail.com>
2016-05-12 10:19:08 -0400

    adds LFLixel_volume

--- py/lightfield/lightfield_geometry.py
+++ py/lightfield/lightfield_geometry.py
@@ -6,39 +6,39 @@ from lightfield_transport import Transport
 
 lib.LFPlaneGeometry_ws.restype = ct.c_float
 lib.LFPlaneGeometry_wt.restype = ct.c_float
+lib.LFLixel_volume.restype = ct.c_float
 
 class LightFieldGeometry(object):
     def __init__(self, 
             plane_geometry,
             angular_plane,
-            optics_to_root,
-            smin = None,
-            smax = None,
-            tmin = None,
-            tmax = None):
+            optics_to_root):
         self.plane_geometry = plane_geometry
         self.angular_plane = angular_plane
         self.optics_to_root = optics_to_root
-        self.smin = smin if smin is not None else 0
-        self.smax = smax if smax is not None else self.plane_geometry.ns
-        self.tmin = tmin if tmin is not None else 0
-        self.tmax = tmax if tmax is not None else self.plane_geometry.nt
 
     @property
     def ns(self):
-        return self.smax - self.smin
+        return self.plane_geometry.ns
 
     @property
     def nt(self):
-        return self.tmax - self.tmin
+        return self.plane_geometry.nt
 
     @property
     def shape(self):
-        return (self.plane_geometry.ns, self.plane_geometry.nt)
+        return (self.ns, self.nt)
 
     @property
     def zeros(self):
         return np.zeros(self.shape, dtype='float32', order='f')
+
+    @property
+    def lixel_volume(self):
+        return lib.LFLixel_volume(ct.pointer(self.plane_geometry),
+                ct.pointer(self.angular_plane),
+                ct.pointer(self.optics_to_root.optics_x),
+                ct.pointer(self.optics_to_root.optics_y))
 
     @property
     def ones(self):
