c01b3126ce4a6198e2da8f793b41007d280a85aa
Madison McGaffin <greyhill@gmail.com>
2016-06-02 14:55:47 -0400

    assume SplineKernel will be working with f32s

--- rs/spline_kernel.rs
+++ rs/spline_kernel.rs
@@ -1,6 +1,6 @@
 extern crate num;
 extern crate byteorder;
-use self::num::{Float};
+use self::num::{Float, ToPrimitive};
 use cl_traits::*;
 use self::byteorder::*;
 
@@ -29,28 +29,28 @@ impl<F: Float> SplineKernel<F> {
     }
 }
 
-impl ClHeader for SplineKernel<f32> {
+impl<F: Float> ClHeader for SplineKernel<F> {
     fn header() -> &'static str {
         include_str!("../cl/spline_kernel_f32.opencl")
     }
 }
 
-impl<> ClBuffer for SplineKernel<f32> {
+impl<F: Float + ToPrimitive> ClBuffer for SplineKernel<F> {
     fn as_cl_bytes(self: &Self, buf: &mut Vec<u8>) -> () {
         match self {
             &SplineKernel::Rect(ref height, ref mag, ref taus) => {
-                buf.write_f32::<LittleEndian>(*height).unwrap();
-                buf.write_f32::<LittleEndian>(*mag).unwrap();
-                buf.write_f32::<LittleEndian>(taus[0]).unwrap();
-                buf.write_f32::<LittleEndian>(taus[1]).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(height).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(mag).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[0]).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[1]).unwrap()).unwrap();
             },
             &SplineKernel::Trapezoid(ref height, ref mag, ref taus) => {
-                buf.write_f32::<LittleEndian>(*height).unwrap();
-                buf.write_f32::<LittleEndian>(*mag).unwrap();
-                buf.write_f32::<LittleEndian>(taus[0]).unwrap();
-                buf.write_f32::<LittleEndian>(taus[1]).unwrap();
-                buf.write_f32::<LittleEndian>(taus[2]).unwrap();
-                buf.write_f32::<LittleEndian>(taus[3]).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(height).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(mag).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[0]).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[1]).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[2]).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[3]).unwrap()).unwrap();
             },
         }
     }
