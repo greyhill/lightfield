ddac69e050ebbc6629dfc94794c308aeebe2d6cd
Madison McGaffin <greyhill@gmail.com>
2016-06-10 17:10:55 -0400

    adds Vec<Lens> serialization

--- rs/lens.rs
+++ rs/lens.rs
@@ -91,6 +91,73 @@ impl<F: Float + FromPrimitive + ToPrimitive> Serialize for Lens<F> {
     }
 }
 
+impl<F: Float + FromPrimitive + ToPrimitive> Serialize for Vec<Lens<F>> {
+    fn from_map(map: &Table) -> Option<Self> {
+        if let Some(&Value::Array(ref arr)) = map.get("lenses") {
+            let mut tr = Vec::new();
+            for it in arr.iter() {
+                if let &Value::Table(ref f) = it {
+                    if let Some(l) = Lens::from_map(f) {
+                        tr.push(l);
+                    }
+                }
+            }
+            Some(tr)
+        } else {
+            None
+        }
+    }
+
+    fn into_map(self: &Self) -> Table {
+        let mut tr = Table::new();
+        let mut vals: Vec<Value> = Vec::new();
+        for it in self.iter() {
+            let lens_table = it.into_map();
+            vals.push(Value::Table(lens_table));
+        }
+        tr.insert("lenses".to_string(), Value::Array(vals));
+        tr
+    }
+}
+
+#[test]
+fn test_read_lens_array_simple() {
+    let test = r#"
+    [[lenses]]
+    center_s = 3.0
+    center_t = -5.2
+    radius_s = 4.0
+    radius_t = 5.2
+    focal_length_s = 12.0
+    focal_length_t = 24.0
+
+    [[lenses]]
+    center_s = 1.0
+    center_t = -2.2
+    radius_s = 3.0
+    radius_t = 4.2
+    focal_length_s = 5.0
+    focal_length_t = 6.0
+    "#;
+
+    let map = Parser::new(test).parse().unwrap();
+    let array: Vec<Lens<f32>> = Vec::<Lens<f32>>::from_map(&map).unwrap();
+
+    assert_eq!(array[0].center_s, 3.0);
+    assert_eq!(array[0].center_t, -5.2);
+    assert_eq!(array[0].radius_s, 4.0);
+    assert_eq!(array[0].radius_t, 5.2);
+    assert_eq!(array[0].focal_length_s, 12.0);
+    assert_eq!(array[0].focal_length_t, 24.0);
+
+    assert_eq!(array[1].center_s, 1.0);
+    assert_eq!(array[1].center_t, -2.2);
+    assert_eq!(array[1].radius_s, 3.0);
+    assert_eq!(array[1].radius_t, 4.2);
+    assert_eq!(array[1].focal_length_s, 5.0);
+    assert_eq!(array[1].focal_length_t, 6.0);
+}
+
 #[test]
 fn test_read_lens() {
     let test = r#"
