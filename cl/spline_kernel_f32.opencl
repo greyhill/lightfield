505f17884596722324c01d9d996c55a0bd50a372
Madison McGaffin <greyhill@gmail.com>
2016-06-14 21:06:39 -0400

    adds QuadSplineKernel to SplineKernel

--- cl/spline_kernel_f32.opencl
+++ cl/spline_kernel_f32.opencl
@@ -16,6 +16,19 @@ struct TrapezoidSplineKernel {
     float tau3;
 };
 
+struct QuadSplineKernel {
+    float height;
+    float magnification;
+    float tau0;
+    float tau1;
+    float tau2;
+    float tau3;
+    float tau4;
+    float tau5;
+    float tau6;
+    float tau7;
+};
+
 float TrapezoidSplineKernel_sample(
         global struct TrapezoidSplineKernel* k,
         const float loc,
@@ -64,4 +77,73 @@ float TrapezoidSplineKernel_integrate(
 
     return k->height * accum;
 }
+
+#define POW2(m) ((m)*(m))
+#define POW3(m) ((m)*(m)*(m))
+float QuadSplineKernel_integrate(
+        global struct QuadSplineKernel* k,
+        const float loc,
+        const float x0,
+        const float x1) {
+    const float t0 = k->tau0 + loc * k->magnification;
+    const float t1 = k->tau1 + loc * k->magnification;
+    const float t2 = k->tau2 + loc * k->magnification;
+    const float t3 = k->tau3 + loc * k->magnification;
+    const float t4 = k->tau4 + loc * k->magnification;
+    const float t5 = k->tau5 + loc * k->magnification;
+    const float t6 = k->tau6 + loc * k->magnification;
+    const float t7 = k->tau7 + loc * k->magnification;
+
+    float accum = 0.f;
+    float c1 = 1.f / ((t1 - t0) * ((t1 - t0) / 2.f + t2 - t1 + (t3 - t2) / 2.f));
+
+    float l = fmax(x0, t0);
+    float r = fmin(x1, t1);
+    if(r > l) {
+        accum += c1 * (POW3(r - t0) - POW3(l - t0)) / 6.f;
+    }
+
+    l = fmax(x0, t1);
+    r = fmin(x1, t2);
+    if(r > l) {
+        accum += c1 * (t1 - t0)*(POW2(r - t1) - POW2(l - t1)) / 2.f;
+        accum += c1 * POW2(t1 - t0) * (r - l) / 2.f;
+    }
+
+    l = fmax(x0, t2);
+    r = fmin(x1, t3);
+    if(r > l) {
+        accum += (r - l);
+        accum -= c1 * (t1 - t0)*(POW3(r - t3) - POW3(l - t3)) / (6.f * (t3 - t2));
+    }
+
+    l = fmax(x0, t3);
+    r = fmin(x1, t4);
+    accum += fmax(0.f, r - l);
+
+    l = fmax(x0, t4);
+    r = fmin(x1, t5);
+    if(r > l) {
+        accum += (r - l);
+        accum -= c1 * (t1 - t0)*(POW3(r - t4) - POW3(l - t4)) / (6.f * (t3 - t2));
+    }
+
+    l = fmax(x0, t5);
+    r = fmin(x1, t6);
+    if(r > l) {
+        accum += c1 * POW2(t1 - t0) / 2.f * (r - l);
+        accum -= c1 * (t1 - t0)/2.f * (POW2(r - t6) - POW2(l - t6));
+    }
+
+    l = fmax(x0, t6);
+    r = fmin(x1, t7);
+    if(r > l) {
+        accum += c1 * (POW3(r - t7) - POW3(l - t7)) / 6.f;
+    }
+
+    return accum * k->height;
+}
+#undef POW2
+#undef POW3
+
 
--- rs/spline_kernel.rs
+++ rs/spline_kernel.rs
@@ -9,6 +9,7 @@ use self::byteorder::*;
 pub enum SplineKernel<F: Float> {
     Rect(F, F, [F; 2]),
     Trapezoid(F, F, [F; 4]),
+    Quad(F, F, [F; 8]),
 }
 
 impl<F: Float> SplineKernel<F> {
@@ -19,11 +20,20 @@ impl<F: Float> SplineKernel<F> {
         SplineKernel::Trapezoid(height, mag, [taus[0], taus[1], taus[2], taus[3]])
     }
 
+    /// sorts taus and returns a SplineKernel
+    pub fn new_quad(height: F, mag: F, taus: &[F]) -> Self {
+        let mut t = taus.to_owned();
+        t.sort_by(|l, r| l.partial_cmp(r).unwrap());
+        SplineKernel::Quad(height, mag, [taus[0], taus[1], taus[2], taus[3],
+                                         taus[4], taus[5], taus[6], taus[7]])
+    }
+
     /// Height of the kernel
     pub fn height(self: &Self) -> F {
         match self {
             &SplineKernel::Rect(ref height, _, _) => height.clone(),
             &SplineKernel::Trapezoid(ref height, _, _) => height.clone(),
+            &SplineKernel::Quad(ref height, _, _) => height.clone(),
         }
     }
 
@@ -32,6 +42,7 @@ impl<F: Float> SplineKernel<F> {
         match self {
             &SplineKernel::Rect(_, ref mag, _) => mag.clone(),
             &SplineKernel::Trapezoid(_, ref mag, _) => mag.clone(),
+            &SplineKernel::Quad(_, ref mag, _) => mag.clone(),
         }
     }
 }
@@ -58,6 +69,18 @@ impl<F: Float + ToPrimitive> ClBuffer for SplineKernel<F> {
                 buf.write_f32::<LittleEndian>(F::to_f32(&taus[1]).unwrap()).unwrap();
                 buf.write_f32::<LittleEndian>(F::to_f32(&taus[2]).unwrap()).unwrap();
                 buf.write_f32::<LittleEndian>(F::to_f32(&taus[3]).unwrap()).unwrap();
+            },
+            &SplineKernel::Quad(ref height, ref mag, ref taus) => {
+                buf.write_f32::<LittleEndian>(F::to_f32(height).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(mag).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[0]).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[1]).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[2]).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[3]).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[4]).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[5]).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[6]).unwrap()).unwrap();
+                buf.write_f32::<LittleEndian>(F::to_f32(&taus[7]).unwrap()).unwrap();
             },
         }
     }
